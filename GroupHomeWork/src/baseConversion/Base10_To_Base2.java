409b36efcfeb9dd30dce5f63b5f37ab1930ef733
sunkanmibakare <sunkanmibakare@192.168.0.16>
2019-09-15 09:54:51 -0400

    This might be a perfect solution to the bug. This conversion from base10 to base2 works for both big and small numbers.

--- GroupHomeWork/src/baseConversion/Base10_To_Base2.java
+++ GroupHomeWork/src/baseConversion/Base10_To_Base2.java
@@ -1,29 +1,51 @@
 package baseConversion;
 
+import java.util.Scanner;
+
 public class Base10_To_Base2 {
-	
+
 	public static void main(String[] args) {
-	
-	// Create a Scanner object
-	java.util.Scanner input = new java.util.Scanner(System.in);
-	
-	// Prompt the user for input
-	System.out.print("Enten an integer: ");
-	int num = input.nextInt();
-	
-	// Create an array to hold result
-	int[] result = new int[num/10];
-	
-	// Convert num to base2
-	for (int i = num, j = result.length-1; i > 0 && j>=0; i /= 2, --j) {
-		result[j] = i % 2;
+
+		// Create a Scanner object
+		Scanner input = new Scanner(System.in);
+
+		// Prompt the user for input
+		System.out.print("Enten an integer value: ");
+		int value = input.nextInt();
+
+		// Print result
+		System.out.print("The binary conversion of " + value + " is ");
+		int[] result = getBinaryConversion(value);
+		for (int i = 0; i < result.length; i++) {
+			System.out.print(result[i]);
+		}
+
 	}
-	
-	// Print result
-	for (int i = 0; i < result.length; i++) {
-		System.out.print(result[i]);
+
+	// Method for converting from Base10 to Base2
+	public static int[] getBinaryConversion(int value) {
+
+		// Create an array to hold result
+		int[] result = new int[getNumberofElements(value)];
+
+		// Convert from Base10 to Base2
+		for (int i = value, j = result.length - 1; i > 0 && j >= 0; i /= 2, --j) {
+			result[j] = i % 2;
+		}
+
+		return result;
 	}
-	
+
+	// Method for finding the number of elements required in the result array.
+	public static int getNumberofElements(int value) {
+
+		int count = 0;
+
+		for (int i = value; i > 0; i /= 2) {
+			count++;
+		}
+
+		return count;
 	}
 
 }
